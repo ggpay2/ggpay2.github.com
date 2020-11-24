770501e24a61febce65fda6d06908c8d145ba9aa
ggpay2 <74987398+ggpay2@users.noreply.github.com>
2020-11-24 23:00:28 +0300

    Update test.py

--- cgi-bin/test.py
+++ cgi-bin/test.py
@@ -1,20 +1,24 @@
+# -*- coding: utf8 -*-
 #!/usr/bin/env python3
 import cgi
+import html
 
 form = cgi.FieldStorage()
-text1 = form.getfirst("TEXT_1", "не задано")
-text2 = form.getfirst("TEXT_2", "не задано")
+text1 = form.getfirst("TEXT_1", "sdfsdf")
+text2 = form.getfirst("TEXT_2", "sfdsdf")
+text1 = html.escape(text1)
+text2 = html.escape(text2)
 
 print("Content-type: text/html\n")
 print("""<!DOCTYPE HTML>
         <html>
         <head>
             <meta charset="utf-8">
-            <title>Обработка данных форм</title>
+            <title>sdfsdfsdf</title>
         </head>
         <body>""")
 
-print("<h1>Обработка данных форм!</h1>")
+print("<h1>sfsdfsdf!</h1>")
 print("<p>TEXT_1: {}</p>".format(text1))
 print("<p>TEXT_2: {}</p>".format(text2))
 
